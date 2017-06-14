5b4a41fbde5ede1821af1fd71bb9620d5e4fafc2
Adrien Dubois <adrien.dubois.be@gmail.com>
2017-06-14 21:17:34 +0200

    Creating bars, users, days & availabilities controllers

---
+++ app/controllers/days_controller.rb
@@ -0,0 +1,19 @@
+class DaysController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,52 @@
 Rails.application.routes.draw do
+  get 'availabilities/index'
+
+  get 'availabilities/show'
+
+  get 'availabilities/create'
+
+  get 'availabilities/edit'
+
+  get 'availabilities/update'
+
+  get 'availabilities/destroy'
+
+  get 'days/index'
+
+  get 'days/show'
+
+  get 'days/create'
+
+  get 'days/edit'
+
+  get 'days/update'
+
+  get 'days/destroy'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/create'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
+  get 'bars/index'
+
+  get 'bars/show'
+
+  get 'bars/create'
+
+  get 'bars/edit'
+
+  get 'bars/update'
+
+  get 'bars/destroy'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
